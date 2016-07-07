69f93951124a19fe413f2ac4b8f9fb7cb22818f3
shikhin <shikhin.arora@gmail.com>
2016-07-07 11:33:29 +0530

    calculator class added

---
+++ app/src/main/java/demo/shikhin/junittesting/Calculator.java
@@ -0,0 +1,23 @@
+package demo.shikhin.junittesting;
+
+/**
+ * Created by home on 7/7/2016.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return 0;
+    }
+
+    public double subtract(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+}
